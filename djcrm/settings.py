693f92028be4d38841cbe9f8de3b90100dba6539
AlexanderTCHK <occasio.rdh@gmail.com>
2021-12-09 08:31:01 +0530

    Preparing to send emails

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -183,3 +183,10 @@ if not DEBUG:
     X_FRAME_OPTIONS = "DENY"
 
     ALLOWED_HOSTS = ["*"]
+
+    # EMAIL_HOST = "smtp.mailgun.org"
+    # EMAIL_HOST_USER = 
+    # EMAIL_HOST_PASSWORD = 
+    # EMAIL_USE_TLS = True
+    # EMAIL_PORT = 587
+    # DEFAULT_FROM_EMAIL = 'djcrm@django-djcrm.com'
